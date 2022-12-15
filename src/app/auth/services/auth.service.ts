296c98077978619ff2d871f0c6596ad8a89d85df
Juan Santana <juanka9755@hotmail.com>
2022-12-15 07:10:23 -0500

    05-HEROES: Guards
    Implementacion de Guards
    Mantener sesión en el local storage

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators'
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators'
 
 import { environment } from 'src/environments/environment';
 import { AuthDto } from '../interfaces/auth.interface';
@@ -21,10 +21,30 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  validateAuth(): Observable<boolean> {
+    
+    if(!localStorage.getItem('id')){
+      return of(false);
+    }
+
+    return this.http.get<AuthDto>(`${this.apiUrl}/usuarios/1`)
+    .pipe(
+      map( auth => {
+        this._auth = auth;
+        return true;
+      })
+    );
+  }
+
   login():Observable<AuthDto>{
     return this.http.get<AuthDto>(`${this.apiUrl}/usuarios/1`)
     .pipe(
-      tap(response => this._auth = response)
+      tap(response => this._auth = response),
+      tap(auth => localStorage.setItem('id', auth.id))
     );
+  }
+
+  logout(){
+    this._auth = undefined;
   }
 }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -26,6 +26,7 @@ export class HomeComponent implements OnInit {
 
   logout(){
     this.router.navigate(['./auth']);
+    this.authService.logout();
   }
 
 }
